1fe1442ee03053c5eb7f5cea247e124e39f5a6ad
haruki <kinikun@outlook.com>
2026-02-27 16:23:14 +0800

    feat: 实现搜索页路由查询参数这一块

--- vue3/src/views/search/composables/query-desuwa.ts
+++ vue3/src/views/search/composables/query-desuwa.ts
@@ -2,6 +2,8 @@ import type { SearchPageSortModeType } from '@/api'
 import { queryKeys, useSearchPageMessageListQuery } from '@/queries'
 import { useQueryClient } from '@tanstack/vue-query'
 import type { SearchPagePageRecoverDataDesuwaType } from './page-recover'
+import { useRoute, useRouter } from 'vue-router'
+import { searchPageRouterQueryParametersKeyConfig } from '@/config'
 
 export const useSearchMessageQueryDesuwa = (data: {
   searchPagePageRecoverDataDesuwa: SearchPagePageRecoverDataDesuwaType
@@ -35,6 +37,26 @@ export const useSearchMessageQueryDesuwa = (data: {
       searchPagePageRecoverData.data.searchmessageQueryPage
   }
 
+  // 根据路由查询参数，初始化搜索
+  const route = useRoute()
+  const router = useRouter()
+  // 获取被封装至Config的键
+  const { search: keySearch } = searchPageRouterQueryParametersKeyConfig
+  // 从路由参数获取数据
+  const routeQueryData = (() => {
+    const search = route.query[keySearch]
+    if (search == null || typeof search !== 'string') {
+      return null
+    }
+    return {
+      search,
+    }
+  })()
+  // 成功获取，根据路由查询参数，初始化搜索
+  if (routeQueryData != null) {
+    searchmessageQuerySearch.value = routeQueryData.search
+  }
+
   // 查询排序模式切换
   // 切换至 created_desc
   const searchmessageQuerySortModeSetToDesc = () => {
@@ -51,6 +73,20 @@ export const useSearchMessageQueryDesuwa = (data: {
   const searchmessageQuerySearchSet = (val: string) => {
     searchmessageQuerySearch.value = val
     searchmessageQueryPage.value = 1
+    // 与route中的查询参数保持一致
+    const newQueryVal = (() => {
+      const newQuery = { ...route.query }
+      if (val === '') {
+        delete newQuery[keySearch]
+      } else {
+        newQuery[keySearch] = val
+      }
+      return newQuery
+    })()
+    router.replace({
+      path: route.path,
+      query: newQueryVal,
+    })
   }
 
   // 查询页数设置
